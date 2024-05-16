ec7a58891d2428cbd36f88f607d508e444b75ad8
tunamaguro <tsu7magu6@gmail.com>
2024-05-16 20:58:57 +0000

    6-2: hw driver

--- wio_examples/examples/6-2-led_and_button_driver.rs
+++ wio_examples/examples/6-2-led_and_button_driver.rs
@@ -24,13 +24,62 @@ fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     let mut pins = wio::Pins::new(peripherals.PORT);
 
-    // TODO: ボタン1を押している間、LEDが点灯するコードを書く
+    let mut led = Led::new(pins.user_led, &mut pins.port);
+    let button1 = Button1::new(pins.button1, &mut pins.port);
 
-    loop {}
+    loop {
+        if button1.is_pressed() {
+            led.turn_on();
+        } else {
+            led.turn_off();
+        }
+    }
 }
 
 // Wio Terminalのボタン1ドライバ
 // TODO: Button1 を実装する
+struct Button1 {
+    pin: Pc26<Input<Floating>>,
+}
+
+impl Button1 {
+    fn new(pin: Pc26<Input<Floating>>, port: &mut Port) -> Self {
+        Button1 {
+            pin: pin.into_floating_input(port),
+        }
+    }
+
+    fn is_pressed(&self) -> bool {
+        self.pin.is_low().unwrap()
+    }
+
+    fn is_released(&self) -> bool {
+        self.pin.is_high().unwrap()
+    }
+}
 
 // Wio TerminalのユーザーLEDドライバ
 // TODO: Led を実装する
+struct Led {
+    pin: Pa15<Output<PushPull>>,
+}
+
+impl Led {
+    fn new(pin: Pa15<Input<Floating>>, port: &mut Port) -> Self {
+        Led {
+            pin: pin.into_push_pull_output(port),
+        }
+    }
+
+    fn turn_on(&mut self) {
+        self.pin.set_high().unwrap()
+    }
+
+    fn turn_off(&mut self) {
+        self.pin.set_low().unwrap()
+    }
+
+    fn toggle(&mut self) {
+        self.pin.toggle();
+    }
+}
