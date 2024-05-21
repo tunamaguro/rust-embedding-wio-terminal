a7ea9c6b1195e8882f04910b6d2815be2f473c75
tunamaguro <tsu7magu6@gmail.com>
2024-05-21 18:06:46 +0000

    light sensor

--- wio_examples/examples/6-6-light_sensor.rs
+++ wio_examples/examples/6-6-light_sensor.rs
@@ -13,12 +13,12 @@ use panic_halt as _;
 use wio_terminal as wio;
 
 use core::fmt::Write;
-use nb;
-use wio::{entry, Pins};
 use wio::hal::clock::GenericClockController;
 use wio::hal::delay::Delay;
 use wio::pac::{CorePeripherals, Peripherals};
 use wio::prelude::*;
+use wio::{entry, Pins};
+use wio_examples::Led;
 
 #[entry]
 fn main() -> ! {
@@ -37,6 +37,12 @@ fn main() -> ! {
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
     // TODO: 光センサ読み取り用の ADC とピンとを初期化する
+    let (mut sensor, mut pd1) = sets.light_sensor.init(
+        peripherals.ADC1,
+        &mut clocks,
+        &mut peripherals.MCLK,
+        &mut sets.port,
+    );
 
     // UARTドライバオブジェクトを初期化する
     let mut serial = sets.uart.init(
@@ -47,8 +53,17 @@ fn main() -> ! {
         &mut sets.port,
     );
 
+    let mut led = Led::new(sets.user_led, &mut sets.port);
+
     loop {
         // TODO: ADC入力を1秒に1回取得して、UARTに出力する
-
+        let value: u16 = nb::block!(sensor.read(&mut pd1)).unwrap();
+        writeln!(&mut serial, "sensor value: {}", value).unwrap();
+        if value > 256 {
+            led.turn_on();
+        } else {
+            led.turn_off();
+        }
+        delay.delay_ms(10_u16);
     }
 }
